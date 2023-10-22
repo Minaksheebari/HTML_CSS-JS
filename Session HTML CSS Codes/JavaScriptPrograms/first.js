54681ded3ed5775099f30575cb59cfce53d4f257
Minakshee Bari <minaksheebari22@gmail.com>
2023-10-22 16:49:53 +0530

    Adding DCS Project website code and Javascript Programs foldel

---
+++ Session HTML CSS Codes/JavaScriptPrograms/first.js
@@ -0,0 +1,37 @@
+// print statement
+console.log("Hi JS")
+
+// JS is single threaded and schynchronous language
+// schynchronous : line by line
+// single threaded : one task at a time 
+
+
+// declaring variable
+// JS remembers it's variables(NOT it's actual value) & functions
+// Execution: memory allocation phase & execution phase
+
+console.log(a) //Output: undefined
+var a=10;
+var a = 20;
+console.log("Value of A=",a);
+
+let b=10;
+console.log("Let B=",b);
+
+const c=10;
+console.log("Const C=",c);
+
+// JS engine : GLobal Object : will consist some of the by default methods of javascript.
+
+
+
+
+
+
+
+
+
+
+
+
+
